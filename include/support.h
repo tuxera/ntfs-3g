c23679552d08e385e20cc70d7077ae4d2cf04b98
!flatcap <!flatcap>
2002-08-22 18:09:47 +0000

    whitespace and include guards
    2002/04/14 13:56:45-00:00 !antona
    cleanup header files.
    
    2001/12/15 05:13:08-00:00 !antona
    Remove atomic ops and add compiler version check.
    
    2001/04/08 03:02:55-00:00 !antona
    Added cvs Id header.
    
    2001/04/02 02:04:37-00:00 !antona
    Everything compiles again! Yey! (Don't know about working though, haven't tried
    it... So be careful...)
    The definitely final find_{first_}attr() functions are in place. Currently
    still no support for attribute lists.
    The two new _RE files contain the C-fied and more or less (more less than more
    actually) cleaned up functions from the ntfs driver. Once they are cleaned
    up (find_attr() is already completed but I left it in the _RE files for future
    reference/educational value) and modified to suit my ideas of how they should
    work, which are not quite the same as the driver way, they will make it into
    attrib.[ch].
    If anyone gives the new code a try, I would be interested in whether it worked
    or not... (-;
    
    2001/03/26 03:42:13-00:00 !antona
    Forgot to add the new support.h
    
    (Logical change 1.5)

---
+++ include/support.h
@@ -0,0 +1,57 @@
+/*
+ * $Id$
+ *
+ * support.h - Useful definitions and macros. Part of the Linux-NTFS project.
+ *
+ * Copyright (c) 2000-2002 Anton Altaparmakov.
+ *
+ * This program/include file is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program/include file is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program (in the main directory of the Linux-NTFS
+ * distribution in the file COPYING); if not, write to the Free Software
+ * Foundation,Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef _NTFS_SUPPORT_H
+#define _NTFS_SUPPORT_H
+
+#include <stddef.h>
+
+/*
+ * Generic macro to convert pointers to values for comparison purposes.
+ */
+#ifndef p2n
+#define p2n(p)          ((ptrdiff_t)((ptrdiff_t*)(p)))
+#endif
+
+/*
+ * The classical min and max macros.
+ */
+#ifndef min
+#define min(a,b)        ((a) <= (b) ? (a) : (b))
+#endif
+
+#ifndef max
+#define max(a,b)        ((a) >= (b) ? (a) : (b))
+#endif
+
+/*
+ * Simple bit operation macros.
+ */
+#define test_bit(bit, var)		((var) & (1 << (bit)))
+#define set_bit(bit, var)		(var) |= 1 << (bit)
+#define clear_bit(bit, var)		(var) &= ~(1 << (bit))
+#define test_and_set_bit(bit, var)	(test_bit(bit, var), set_bit(bit, var))
+#define test_and_clear_bit(bit, var)	(test_bit(bit, var), clear_bit(bit, var))
+
+#endif /* defined _NTFS_SUPPORT_H */
+
