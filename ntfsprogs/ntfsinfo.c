177ad3b69326211906a599cc5d0ddf7b249bcc08
flatcap.org!ntfs <flatcap.org!ntfs>
2003-10-11 15:36:14 +0000

    slight rearrangement to group standard options
    (Logical change 1.200)

--- ntfsprogs/ntfsinfo.c
+++ ntfsprogs/ntfsinfo.c
@@ -83,14 +83,14 @@ void usage (void)
 	printf ("\nUsage: %s [options] -d dev\n"
 		"    -d dev  --device dev The ntfs volume to display information about\n"
 		"    -i num  --inode num  Display information about this inode\n"
+		"    -t      --epochtime  Report all timestamps as \"Thu Jan  1 00:00:00 1970\"\n"
+		"    -T      --notime     Don't report timestamps at all\n"
 		"\n"
 		"    -f      --force      Use less caution\n"
 		"    -q      --quiet      Less output\n"
 		"    -v      --verbose    More output\n"
 		"    -V      --version    Display version information\n"
-		"    -h      --help       Display this help\n\n"
-		"    -t      --epochtime  Report all timestamps as \"Thu Jan  1 00:00:00 1970\"\n"
-		"    -T      --notime     Don't report timestamps at all\n",
+		"    -h      --help       Display this help\n\n",
 		EXEC_NAME);
 	printf ("%s%s\n", ntfs_bugs, ntfs_home);
 }
