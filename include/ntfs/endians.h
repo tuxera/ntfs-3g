c1e3dc03beb51160615e577c0763a82b8f84cb69
cantab.net!aia21 <cantab.net!aia21>
2004-02-25 14:21:14 +0000

    Rename: include/endians.h -> include/ntfs/endians.h
    (Logical change 1.280)

---
+++ include/ntfs/endians.h
@@ -0,0 +1,158 @@
+/*
+ * endians.h - Definitions related to handling of byte ordering. Part of the
+ *	       Linux-NTFS project.
+ *
+ * Copyright (c) 2000-2004 Anton Altaparmakov
+ *
+ * This program/include file is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program/include file is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program (in the main directory of the Linux-NTFS
+ * distribution in the file COPYING); if not, write to the Free Software
+ * Foundation,Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef _NTFS_ENDIANS_H
+#define _NTFS_ENDIANS_H
+
+#include "config.h"
+
+/*
+ * Notes:
+ *
+ *	We define the conversion functions including typecasts since the
+ * defaults don't necessarily perform appropriate typecasts.
+ *	Also, using our own functions means that we can change them if it
+ * turns out that we do need to use the unaligned access macros on
+ * architectures requiring aligned memory accesses...
+ */
+
+#ifdef HAVE_ENDIAN_H
+#	include <endian.h>
+#endif
+#ifdef HAVE_SYS_ENDIAN_H
+#	include <sys/endian.h>
+#endif
+#ifdef HAVE_MACHINE_ENDIAN_H
+#	include <machine/endian.h>
+#endif
+#ifndef __BYTE_ORDER
+#	ifdef _BYTE_ORDER
+#		define __BYTE_ORDER _BYTE_ORDER
+#		define __LITTLE_ENDIAN _LITTLE_ENDIAN
+#		define __BIG_ENDIAN _BIG_ENDIAN
+#	else
+#		ifdef BYTE_ORDER
+#			define __BYTE_ORDER BYTE_ORDER
+#			define __LITTLE_ENDIAN LITTLE_ENDIAN
+#			define __BIG_ENDIAN BIG_ENDIAN
+#		endif
+#	endif
+#endif
+#ifdef HAVE_BYTESWAP_H
+#	include <byteswap.h>
+#endif
+
+#if (__BYTE_ORDER == __LITTLE_ENDIAN)
+
+#define __le16_to_cpu(x) (x)
+#define __le32_to_cpu(x) (x)
+#define __le64_to_cpu(x) (x)
+
+#define __cpu_to_le16(x) (x)
+#define __cpu_to_le32(x) (x)
+#define __cpu_to_le64(x) (x)
+
+#define __constant_le16_to_cpu(x) (x)
+#define __constant_le32_to_cpu(x) (x)
+#define __constant_le64_to_cpu(x) (x)
+
+#define __constant_cpu_to_le16(x) (x)
+#define __constant_cpu_to_le32(x) (x)
+#define __constant_cpu_to_le64(x) (x)
+
+#elif (__BYTE_ORDER == __BIG_ENDIAN)
+
+#define __le16_to_cpu(x) bswap_16(x)
+#define __le32_to_cpu(x) bswap_32(x)
+#define __le64_to_cpu(x) bswap_64(x)
+
+#define __cpu_to_le16(x) bswap_16(x)
+#define __cpu_to_le32(x) bswap_32(x)
+#define __cpu_to_le64(x) bswap_64(x)
+
+#define __constant_le16_to_cpu(x) __bswap_constant_16(x)
+#define __constant_le32_to_cpu(x) __bswap_constant_32(x)
+#define __constant_le64_to_cpu(x) __bswap_constant_64(x)
+
+#define __constant_cpu_to_le16(x) __bswap_constant_16(x)
+#define __constant_cpu_to_le32(x) __bswap_constant_32(x)
+#define __constant_cpu_to_le64(x) __bswap_constant_64(x)
+
+#else
+
+#error "You must define __BYTE_ORDER to be __LITTLE_ENDIAN or __BIG_ENDIAN"
+
+#endif
+
+/* Unsigned from LE to CPU conversion. */
+
+#define le16_to_cpu(x)		(u16)__le16_to_cpu((u16)(x))
+#define le32_to_cpu(x)		(u32)__le32_to_cpu((u32)(x))
+#define le64_to_cpu(x)		(u64)__le64_to_cpu((u64)(x))
+
+#define le16_to_cpup(x)		(u16)__le16_to_cpu(*(u16*)(x))
+#define le32_to_cpup(x)		(u32)__le32_to_cpu(*(u32*)(x))
+#define le64_to_cpup(x)		(u64)__le64_to_cpu(*(u64*)(x))
+
+/* Signed from LE to CPU conversion. */
+
+#define sle16_to_cpu(x)		(s16)__le16_to_cpu((s16)(x))
+#define sle32_to_cpu(x)		(s32)__le32_to_cpu((s32)(x))
+#define sle64_to_cpu(x)		(s64)__le64_to_cpu((s64)(x))
+
+#define sle16_to_cpup(x)	(s16)__le16_to_cpu(*(s16*)(x))
+#define sle32_to_cpup(x)	(s32)__le32_to_cpu(*(s32*)(x))
+#define sle64_to_cpup(x)	(s64)__le64_to_cpu(*(s64*)(x))
+
+/* Unsigned from CPU to LE conversion. */
+
+#define cpu_to_le16(x)		(u16)__cpu_to_le16((u16)(x))
+#define cpu_to_le32(x)		(u32)__cpu_to_le32((u32)(x))
+#define cpu_to_le64(x)		(u64)__cpu_to_le64((u64)(x))
+
+#define cpu_to_le16p(x)		(u16)__cpu_to_le16(*(u16*)(x))
+#define cpu_to_le32p(x)		(u32)__cpu_to_le32(*(u32*)(x))
+#define cpu_to_le64p(x)		(u64)__cpu_to_le64(*(u64*)(x))
+
+/* Signed from CPU to LE conversion. */
+
+#define scpu_to_le16(x)		(s16)__cpu_to_le16((s16)(x))
+#define scpu_to_le32(x)		(s32)__cpu_to_le32((s32)(x))
+#define scpu_to_le64(x)		(s64)__cpu_to_le64((s64)(x))
+
+#define scpu_to_le16p(x)	(s16)__cpu_to_le16(*(s16*)(x))
+#define scpu_to_le32p(x)	(s32)__cpu_to_le32(*(s32*)(x))
+#define scpu_to_le64p(x)	(s64)__cpu_to_le64(*(s64*)(x))
+
+/*
+ * Constant endianness conversion defines.
+ */
+#define const_le16_to_cpu(x)	__constant_le16_to_cpu(x)
+#define const_le32_to_cpu(x)	__constant_le32_to_cpu(x)
+#define const_le64_to_cpu(x)	__constant_le64_to_cpu(x)
+
+#define const_cpu_to_le16(x)	__constant_cpu_to_le16(x)
+#define const_cpu_to_le32(x)	__constant_cpu_to_le32(x)
+#define const_cpu_to_le64(x)	__constant_cpu_to_le64(x)
+
+#endif /* defined _NTFS_ENDIANS_H */
+
