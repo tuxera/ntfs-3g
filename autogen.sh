73fdc0ebd95e42800fc1ac5b6f1e8b11a032e916
cantab.net!aia21 <cantab.net!aia21>
2004-03-05 11:04:40 +0000

    Update to use autoreconf.
    (Logical change 1.294)

--- autogen.sh
+++ autogen.sh
@@ -1,52 +1,31 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-DIE=0
-
-if test x$srcdir = x; then
-  srcdir=.
+if test "$srcdir" == ""; then
+	srcdir=.
+elif test "$srcdir" != "."; then
+	pushd $srcdir > /dev/null
 fi
 
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: You must have \`autoconf' installed to compile Linux-NTFS."
-  echo "Download the appropriate package for your distribution,"
-  echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-  DIE=1
-}
-
-(grep "^AM_PROG_LIBTOOL" $srcdir/configure.ac >/dev/null) && {
-  (libtool --version) < /dev/null > /dev/null 2>&1 || {
+(autoreconf --version) < /dev/null > /dev/null 2>&1 || {
+  (autoconf --version) < /dev/null > /dev/null 2>&1 || {
     echo
-    echo "**Error**: You must have \`libtool' installed to compile Linux-NTFS."
-    echo "Get ftp://ftp.gnu.org/pub/gnu/libtool-1.2d.tar.gz"
-    echo "(or a newer version if it is available)"
-    DIE=1
+    echo "**Error**: You must have the GNU Build System (autoconf, automake, "
+    echo "libtool, etc) to update the ntfsprogs build system.  Download the "
+    echo "appropriate packages for your distribution, or get the source "
+    echo "tar balls from ftp://ftp.gnu.org/pub/gnu/."
+    exit 1
   }
-}
-
-(automake --version) < /dev/null > /dev/null 2>&1 || {
   echo
-  echo "**Error**: You must have \`automake' installed to compile Linux-NTFS."
-  echo "Get ftp://ftp.gnu.org/pub/gnu/automake/automake-1.3.tar.gz"
-  echo "(or a newer version if it is available)"
-  DIE=1
-  NO_AUTOMAKE=yes
-}
-
-# if no automake, don't bother testing for aclocal
-test -n "$NO_AUTOMAKE" || (aclocal --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: Missing \`aclocal'.  The version of \`automake'"
-  echo "installed doesn't appear recent enough."
-  echo "Get ftp://ftp.gnu.org/pub/gnu/automake/automake-1.3.tar.gz"
-  echo "(or a newer version if it is available)"
-  DIE=1
-}
-
-if test "$DIE" -eq 1; then
+  echo "**Error**: Your version of autoconf is too old (you need 2.57) to "
+  echo "update the ntfsprogs build system.  Download the appropriate "
+  echo "updated package for your distribution, or get the source tar ball "
+  echo "from ftp://ftp.gnu.org/pub/gnu/."
   exit 1
-fi
+}
+
+echo Running autoreconf --verbose --install --warnings=all
+autoreconf --verbose --install --warnings=all
 
 if test -z "$*"; then
   echo "**Warning**: I am going to run \`configure' with no arguments."
@@ -55,63 +34,18 @@ if test -z "$*"; then
   echo
 fi
 
-case $CC in
-xlc )
-  am_opt=--include-deps;;
-esac
+conf_flags="--enable-maintainer-mode --enable-compile-warnings"
 
-for coin in `find $srcdir -name configure.ac -print`
-do 
-  dr=`dirname $coin`
-  if test -f $dr/NO-AUTO-GEN; then
-    echo skipping $dr -- flagged as no auto-gen
-  else
-    echo processing $dr
-    macrodirs=`sed -n -e 's,AM_ACLOCAL_INCLUDE(\(.*\)),\1,gp' < $coin`
-    ( cd $dr
-      macrosdir=`find . -name macros -print`
-      aclocalinclude="$ACLOCAL_FLAGS"
-      for k in $macrodirs; do
-  	if test -d $k; then
-          aclocalinclude="$aclocalinclude -I $k"
-        fi
-      done
-      if grep "^AM_PROG_LIBTOOL" configure.ac >/dev/null; then
-	if test -z "$NO_LIBTOOLIZE" ; then 
-	  echo "Running libtoolize..."
-	  libtoolize --force --copy
-	fi
-      fi
-      echo "Running aclocal $aclocalinclude ..."
-      aclocal $aclocalinclude || {
-	echo
-	echo "**Error**: aclocal failed. This may mean that you have not"
-	echo "installed all of the packages you need, or you may need to"
-	echo "set ACLOCAL_FLAGS to include \"-I \$prefix/share/aclocal\""
-	echo "for the prefix where you installed the packages whose"
-	echo "macros were not found"
-	exit 1
-      }
+echo Running $srcdir/configure $conf_flags "$@" ...
+$srcdir/configure $conf_flags "$@" \
+	&& echo Now type \`make\' to compile ntfsprogs. || err=1
 
-      if grep "^AM_CONFIG_HEADER" configure.ac >/dev/null; then
-	echo "Running autoheader..."
-	autoheader || { echo "**Error**: autoheader failed."; exit 1; }
-      fi
-      echo "Running automake --gnu $am_opt ..."
-      automake --add-missing --gnu $am_opt ||
-	{ echo "**Error**: automake failed."; exit 1; }
-      echo "Running autoconf ..."
-      autoconf || { echo "**Error**: autoconf failed."; exit 1; }
-    ) || exit 1
-  fi
-done
-
-conf_flags="--enable-maintainer-mode --enable-compile-warnings" #--enable-iso-c
-
-if test x$NOCONFIGURE = x; then
-  echo Running $srcdir/configure $conf_flags "$@" ...
-  $srcdir/configure $conf_flags "$@" \
-  && echo Now type \`make\' to compile $PKG_NAME || exit 1
-else
-  echo Skipping configure process.
+if test "$srcdir" != "."; then
+	popd > /dev/null
 fi
+
+if test "$err" == "1"; then
+	exit 1
+fi
+
+exit 0
