d2ea0b8741e8dba7aada024c6d5a00407abfeb32
cantab.net!aia21 <cantab.net!aia21>
2004-03-24 10:36:31 +0000

    Add exports for ntfs_device_heads_get() and ntfs_device_sectors_per_track_get().
    (Logical change 1.345)

--- include/ntfs/device.h
+++ include/ntfs/device.h
@@ -108,7 +108,8 @@ extern s64 ntfs_cluster_write(const ntfs_volume *vol, const s64 lcn,
 		const s64 count, const void *b);
 
 extern s64 ntfs_device_size_get(struct ntfs_device *dev, int block_size);
-
 extern s64 ntfs_device_partition_start_sector_get(struct ntfs_device *dev);
+extern int ntfs_device_heads_get(struct ntfs_device *dev);
+extern int ntfs_device_sectors_per_track_get(struct ntfs_device *dev);
 
 #endif /* defined _NTFS_DEVICE_H */
