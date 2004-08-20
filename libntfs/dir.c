86323fac66f6002198bc94b91feef0191f3bf7fd
!uv <!uv>
2004-08-20 12:04:47 +0000

    Comment #includes by configure's #ifdef HAVE_*
    (Logical change 1.506)

--- libntfs/dir.c
+++ libntfs/dir.c
@@ -21,9 +21,15 @@
 
 #include "config.h"
 
-#include <stdlib.h>
-#include <errno.h>
-#include <string.h>
+#ifdef HAVE_STDLIB_H
+#	include <stdlib.h>
+#endif
+#ifdef HAVE_ERRNO_H
+#	include <errno.h>
+#endif
+#ifdef HAVE_STRING_H
+#	include <string.h>
+#endif
 
 #include "types.h"
 #include "debug.h"
