0f3a85c8e32d634f031474038f57336332d5311e
antona <antona>
2005-08-19 16:01:15 +0000

    Only include sys/ioctl.h in libntfs/device.c if ./configure detected it to be present.  (Christophe)

--- libntfs/device.c
+++ libntfs/device.c
@@ -29,7 +29,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <sys/ioctl.h>
+#ifdef HAVE_SYS_IOCTL_H
+#	include <sys/ioctl.h>
+#endif
 #ifdef HAVE_LINUX_FD_H
 #	include <linux/fd.h>
 #endif
