531305cc1e43afe69c99fba45f0055b2bd6c6fb6
flatcap <flatcap>
2005-09-22 16:41:43 +0000

    Christophe: add easily accessible version info to libntfs

---
+++ libntfs/version.c
@@ -0,0 +1,41 @@
+/**
+ * version.c - Info about the NTFS library. Part of the Linux-NTFS project.
+ *
+ * Copyright (c) 2005 Richard Russon
+ *
+ * This program/include file is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program/include file is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program (in the main directory of the Linux-NTFS
+ * distribution in the file COPYING); if not, write to the Free Software
+ * Foundation,Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "version.h"
+
+#ifndef VERSION
+#define VERSION "unknown"
+#endif
+
+/**
+ * ntfs_get_library_version - Version number of the library release
+ *
+ * Returns a text string representing the release of libntfs, e.g. "1.9.4".
+ */
+const char * ntfs_get_library_version (void)
+{
+	return VERSION;
+}
+
