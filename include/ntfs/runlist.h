8ddea064b9d085df009f2d8ce2eccf5be63466fa
aia21 <aia21>
2006-10-27 09:04:08 +0000

    Revert 1.13 commit from Yuval: Gratuitous API/ABI change.

--- include/ntfs/runlist.h
+++ include/ntfs/runlist.h
@@ -62,8 +62,13 @@ extern runlist_element *ntfs_runlists_merge(runlist_element *drl,
 extern runlist_element *ntfs_mapping_pairs_decompress(const ntfs_volume *vol,
 		const ATTR_RECORD *attr, runlist_element *old_rl);
 
+extern int ntfs_get_nr_significant_bytes(const s64 n);
+
 extern int ntfs_get_size_for_mapping_pairs(const ntfs_volume *vol,
 		const runlist_element *rl, const VCN start_vcn);
+
+extern int ntfs_write_significant_bytes(u8 *dst, const u8 *dst_max,
+		const s64 n);
 
 extern int ntfs_mapping_pairs_build(const ntfs_volume *vol, u8 *dst,
 		const int dst_len, const runlist_element *rl,
