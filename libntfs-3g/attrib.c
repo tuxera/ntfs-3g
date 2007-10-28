061c6dd9d853c29f7c2851135e61e24a41900155
szaka <szaka>
2007-10-28 11:13:00 +0000

    NAttrSetFlag(): don't log bogus errors

--- libntfs-3g/attrib.c
+++ libntfs-3g/attrib.c
@@ -72,7 +72,7 @@ static void NAttrSetFlag(ntfs_attr *na, FILE_ATTR_FLAGS flag)
 	if (na->type == AT_DATA && na->name == AT_UNNAMED)
 		na->ni->flags |= flag;
 	else
-		ntfs_log_error("Denied setting flag %d for not unnamed data "
+		ntfs_log_trace("Denied setting flag %d for not unnamed data "
 			       "attribute\n", flag);
 }
 
