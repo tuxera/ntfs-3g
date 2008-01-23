ce46eba9793025f8b555326a9bc6909c3fa14fdd
szaka <szaka>
2008-01-23 21:32:27 +0000

    remove _FILE_OFFSET_BITS define/check since it's in config.h (Alon Bar-Lev)

--- include/fuse-lite/fuse_common.h
+++ include/fuse-lite/fuse_common.h
@@ -27,11 +27,6 @@
 #define FUSE_MAKE_VERSION(maj, min)  ((maj) * 10 + (min))
 #define FUSE_VERSION FUSE_MAKE_VERSION(FUSE_MAJOR_VERSION, FUSE_MINOR_VERSION)
 
-/* This interface uses 64 bit off_t */
-#if _FILE_OFFSET_BITS != 64
-#error Please add -D_FILE_OFFSET_BITS=64 to your compile flags!
-#endif
-
 #ifdef __cplusplus
 extern "C" {
 #endif
