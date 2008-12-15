f7a15b70418ad1d8dcda40c4921fd52c539fe3e3
szaka <szaka>
2008-12-15 03:10:49 +0000

    uninline reply_err()

--- libfuse-lite/fuse.c
+++ libfuse-lite/fuse.c
@@ -1024,7 +1024,7 @@ static struct fuse *req_fuse_prepare(fuse_req_t req)
     return c->ctx.fuse;
 }
 
-static inline void reply_err(fuse_req_t req, int err)
+static void reply_err(fuse_req_t req, int err)
 {
     /* fuse_reply_err() uses non-negated errno values */
     fuse_reply_err(req, -err);
