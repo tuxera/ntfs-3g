08dccd58fb8b1f978516fec4f1b29ce964fd26f1
szaka <szaka>
2009-07-21 12:55:08 +0000

    fix format breaking new lines in error messages

--- libntfs-3g/security.c
+++ libntfs-3g/security.c
@@ -333,7 +333,7 @@ int ntfs_sd_add_everyone(ntfs_inode *ni)
 	ret = ntfs_attr_add(ni, AT_SECURITY_DESCRIPTOR, AT_UNNAMED, 0, (u8 *)sd,
 			    sd_len);
 	if (ret)
-		ntfs_log_perror("Failed to add SECURITY_DESCRIPTOR\n");
+		ntfs_log_perror("Failed to add SECURITY_DESCRIPTOR");
 	
 	free(sd);
 	return ret;
