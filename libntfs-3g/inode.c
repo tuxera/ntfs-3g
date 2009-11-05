9901ea35320aeee40f36647ed3cb28b1a523221a
jpandre <jpandre>
2009-11-05 11:44:38 +0000

    ntfs_inode_open: don't log "No STANDARD_INFORMATION" for extended MFT records

--- libntfs-3g/inode.c
+++ libntfs-3g/inode.c
@@ -182,8 +182,9 @@ ntfs_inode *ntfs_inode_open(ntfs_volume *vol, const MFT_REF mref)
 	/* Receive some basic information about inode. */
 	if (ntfs_attr_lookup(AT_STANDARD_INFORMATION, AT_UNNAMED,
 				0, CASE_SENSITIVE, 0, NULL, 0, ctx)) {
-		ntfs_log_perror("No STANDARD_INFORMATION in base record %lld",
-				(long long)MREF(mref));
+		if (!ni->mrec->base_mft_record)
+			ntfs_log_perror("No STANDARD_INFORMATION in base record"
+					" %lld", (long long)MREF(mref));
 		goto put_err_out;
 	}
 	std_info = (STANDARD_INFORMATION *)((u8 *)ctx->attr +
