fc989c67b52f89a1cf3803d7814680dace686312
Erik Larsson <mechie@users.sourceforge.net>
2010-11-22 10:52:18 +0100

    Removed the automatic 'configure' that happens in autogen.sh. It was really annoying me.

--- autogen.sh
+++ autogen.sh
@@ -31,16 +31,6 @@ autoreconf --force --verbose --install
 # current time stamp rather than the time stamp at check in time.
 touch config.h.in
 
-if test -z "$*"; then
-  echo "**Warning**: I am going to run \`configure' with no arguments."
-  echo "If you wish to pass any to it, please specify them on the"
-  echo \`$0\'" command line."
-  echo
-fi
-
-echo Running $srcdir/configure "$@" ...
-$srcdir/configure "$@" && echo Now type \`make\' to compile ntfsprogs. || err=1
-
 if test "$srcdir" != "."; then
 	popd > /dev/null
 fi
