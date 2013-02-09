44fbf02a97fdd1b0eecaf87e806b4a09a98fe235
Jean-Pierre André <jpandre@users.sourceforge.net>
2013-02-09 11:22:41 +0100

    Upgraded the Win32 interface for use with ntfsprogs
    Upgrade the Win32 interface (win32_io.c) which was designed for Cygwin
    so that it can be used for using the ntfsprogs utilities on native Windows.
    Two new entries are added for truncating a file and creating a sparse
    file, both of which not being supported through msvcrt.dll.

--- ntfsprogs/utils.c
+++ ntfsprogs/utils.c
@@ -208,7 +208,7 @@ int utils_valid_device(const char *name, int force)
 	unsigned long mnt_flags = 0;
 	struct stat st;
 
-#ifdef __CYGWIN32__
+#if defined(HAVE_WINDOWS_H) | defined(__CYGWIN32__) 
 	/* FIXME: This doesn't work for Cygwin, so just return success. */
 	return 1;
 #endif
