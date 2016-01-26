02ce4947dfdb5aa9ed936f8187faa47f506d4d20
Erik Larsson <mechie@users.sourceforge.net>
2016-01-26 07:55:45 +0100

    ntfs-3g.c: Fix raw usage of inode time fields without byteswapping.

--- src/ntfs-3g.c
+++ src/ntfs-3g.c
@@ -568,7 +568,7 @@ static int ntfs_macfuse_getxtimes(const char *org_path,
 	}
 	
 	/* We have no backup timestamp in NTFS. */
-	crtime->tv_sec = ni->creation_time;
+	crtime->tv_sec = sle64_to_cpu(ni->creation_time);
 exit:
 	if (ntfs_inode_close(ni))
 		set_fuse_error(&res);
@@ -590,7 +590,7 @@ int ntfs_macfuse_setcrtime(const char *path, const struct timespec *tv)
 		return -errno;
 	
 	if (tv) {
-		ni->creation_time = tv->tv_sec;
+		ni->creation_time = cpu_to_sle64(tv->tv_sec);
 		ntfs_fuse_update_times(ni, NTFS_UPDATE_CTIME);
 	}
 
@@ -632,7 +632,7 @@ int ntfs_macfuse_setchgtime(const char *path, const struct timespec *tv)
 		return -errno;
 
 	if (tv) {
-		ni->last_mft_change_time = tv->tv_sec;
+		ni->last_mft_change_time = cpu_to_sle64(tv->tv_sec);
 		ntfs_fuse_update_times(ni, 0);
 	}
 
