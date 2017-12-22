85e208176ffd16ce9a51a9f534b194f0ebbde6e3
Jean-Pierre André <jpandre@users.sourceforge.net>
2017-12-22 11:29:00 +0100

    Stopped checking matches of MFTMirr against MFT at record 16
    Since its 2017 edition, Windows 10 has stopped mirroring $MFT to the
    full size of $MFTMirr leading to mounts of partitions with big clusters
    to be rejected because of mismatches. With this patch, only 16 records
    are checked, though mirroring is still done for all records in $MFTMirr.

--- libntfs-3g/volume.c
+++ libntfs-3g/volume.c
@@ -959,7 +959,8 @@ ntfs_volume *ntfs_device_mount(struct ntfs_device *dev, ntfs_mount_flags flags)
 		vol->mftmirr_size = l;
 	}
 	ntfs_log_debug("Comparing $MFTMirr to $MFT...\n");
-	for (i = 0; i < vol->mftmirr_size; ++i) {
+		/* Windows 10 does not update the full $MFTMirr any more */
+	for (i = 0; (i < vol->mftmirr_size) && (i < FILE_first_user); ++i) {
 		MFT_RECORD *mrec, *mrec2;
 		const char *ESTR[12] = { "$MFT", "$MFTMirr", "$LogFile",
 			"$Volume", "$AttrDef", "root directory", "$Bitmap",
