e021cbe3d7a42963110e4561f2a1327614c20a00
Erik Larsson <erik@tuxera.com>
2026-04-24 12:06:24 +0300

    compress.c: Fix incorrect comparison in destination bounds check.
    When checking whether 'dest' was at or past the end of the destination
    buffer we were checking if 'dest' was larger then 'dest_sb_end', however
    'dest_sb_end' is already past the end of the destination buffer since
    it is constructed with 'dest + dest_size', i.e. dereferencing
    'dest_sb_end' is invalid. Fixed by using '>=' instead of '>'.
    
    Thanks to Alexandro Calo' from Nozomi Networks Labs for reporting this
    issue.

--- libntfs-3g/compress.c
+++ libntfs-3g/compress.c
@@ -558,7 +558,7 @@ do_next_tag:
 				goto do_next_sb;
 			}
 			/* Check we are still in range. */
-			if (cb > cb_sb_end || dest > dest_sb_end)
+			if (cb > cb_sb_end || dest >= dest_sb_end)
 				goto return_overflow;
 			/* Get the next tag and advance to first token. */
 			tag = *cb++;
@@ -569,7 +569,7 @@ do_next_tag:
 				u8 *dest_back_addr;
 
 				/* Check if we are done / still in range. */
-				if (cb >= cb_sb_end || dest > dest_sb_end)
+				if (cb >= cb_sb_end || dest >= dest_sb_end)
 					break;
 				/* Determine token type and parse
 				 * appropriately. */
