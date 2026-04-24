6c67a098af01218b995cf4d3e88b84e0652a4d3c
Erik Larsson <erik@tuxera.com>
2026-04-24 06:26:42 +0300

    compress.c: Indent manually unrolled loops for improved readability.
    The code was written in an "unrolled loop" fashion but logically
    consists of an outer loop and an inner loop, so indent these logically
    distinct loops for improved readability.
    Assembly output has been verified to be logically identical before/after
    this modification with clang 21/x86_64.

--- libntfs-3g/compress.c
+++ libntfs-3g/compress.c
@@ -482,150 +482,181 @@ static int ntfs_decompress(u8 *dest, const u32 dest_size,
 
 	ntfs_log_trace("Entering, cb_size = 0x%x.\n", (unsigned)cb_size);
 do_next_sb:
-	ntfs_log_debug("Beginning sub-block at offset = %d in the cb.\n",
-			(int)(cb - cb_start));
-	/*
-	 * Have we reached the end of the compression block or the end of the
-	 * decompressed data?  The latter can happen for example if the current
-	 * position in the compression block is one byte before its end so the
-	 * first two checks do not detect it.
-	 */
-	if (cb == cb_end || !le16_to_cpup((le16*)cb) || dest == dest_end) {
-		if (dest_end > dest)
-			memset(dest, 0, dest_end - dest);
-		ntfs_log_debug("Completed. Returning success (0).\n");
-		return 0;
-	}
-	/* Setup offset for the current sub-block destination. */
-	dest_sb_start = dest;
-	dest_sb_end = dest + NTFS_SB_SIZE;
-	/* Check that we are still within allowed boundaries. */
-	if (dest_sb_end > dest_end)
-		goto return_overflow;
-	/* Does the minimum size of a compressed sb overflow valid range? */
-	if (cb + 6 > cb_end)
-		goto return_overflow;
-	/* Setup the current sub-block source pointers and validate range. */
-	cb_sb_start = cb;
-	cb_sb_end = cb_sb_start + (le16_to_cpup((le16*)cb) & NTFS_SB_SIZE_MASK)
-			+ 3;
-	if (cb_sb_end > cb_end)
-		goto return_overflow;
-	/* Now, we are ready to process the current sub-block (sb). */
-	if (!(le16_to_cpup((le16*)cb) & NTFS_SB_IS_COMPRESSED)) {
-		ntfs_log_debug("Found uncompressed sub-block.\n");
-		/* This sb is not compressed, just copy it into destination. */
-		/* Advance source position to first data byte. */
-		cb += 2;
-		/* An uncompressed sb must be full size. */
-		if (cb_sb_end - cb != NTFS_SB_SIZE)
+	{
+		ntfs_log_debug("Beginning sub-block at offset = %d in the "
+				"cb.\n",
+				(int)(cb - cb_start));
+		/*
+		 * Have we reached the end of the compression block or the end
+		 * of the decompressed data?  The latter can happen for example
+		 * if the current position in the compression block is one byte
+		 * before its end so the first two checks do not detect it.
+		 */
+		if (cb == cb_end || !le16_to_cpup((le16*)cb) ||
+				dest == dest_end)
+		{
+			if (dest_end > dest)
+				memset(dest, 0, dest_end - dest);
+			ntfs_log_debug("Completed. Returning success (0).\n");
+			return 0;
+		}
+		/* Setup offset for the current sub-block destination. */
+		dest_sb_start = dest;
+		dest_sb_end = dest + NTFS_SB_SIZE;
+		/* Check that we are still within allowed boundaries. */
+		if (dest_sb_end > dest_end)
 			goto return_overflow;
-		/* Copy the block and advance the source position. */
-		memcpy(dest, cb, NTFS_SB_SIZE);
-		cb += NTFS_SB_SIZE;
-		/* Advance destination position to next sub-block. */
-		dest += NTFS_SB_SIZE;
-		goto do_next_sb;
-	}
-	ntfs_log_debug("Found compressed sub-block.\n");
-	/* This sb is compressed, decompress it into destination. */
-	/* Forward to the first tag in the sub-block. */
-	cb += 2;
+		/* Does the minimum size of a compressed sb overflow valid
+		 * range? */
+		if (cb + 6 > cb_end)
+			goto return_overflow;
+		/* Setup the current sub-block source pointers and validate
+		 * range. */
+		cb_sb_start = cb;
+		cb_sb_end = cb_sb_start
+				+ (le16_to_cpup((le16*)cb) & NTFS_SB_SIZE_MASK)
+				+ 3;
+		if (cb_sb_end > cb_end)
+			goto return_overflow;
+		/* Now, we are ready to process the current sub-block (sb). */
+		if (!(le16_to_cpup((le16*)cb) & NTFS_SB_IS_COMPRESSED)) {
+			ntfs_log_debug("Found uncompressed sub-block.\n");
+			/* This sb is not compressed, just copy it into
+			 * destination. */
+			/* Advance source position to first data byte. */
+			cb += 2;
+			/* An uncompressed sb must be full size. */
+			if (cb_sb_end - cb != NTFS_SB_SIZE)
+				goto return_overflow;
+			/* Copy the block and advance the source position. */
+			memcpy(dest, cb, NTFS_SB_SIZE);
+			cb += NTFS_SB_SIZE;
+			/* Advance destination position to next sub-block. */
+			dest += NTFS_SB_SIZE;
+			goto do_next_sb;
+		}
+		ntfs_log_debug("Found compressed sub-block.\n");
+		/* This sb is compressed, decompress it into destination. */
+		/* Forward to the first tag in the sub-block. */
+		cb += 2;
 do_next_tag:
-	if (cb == cb_sb_end) {
-		/* Check if the decompressed sub-block was not full-length. */
-		if (dest < dest_sb_end) {
-			int nr_bytes = dest_sb_end - dest;
+		{
+			if (cb == cb_sb_end) {
+				/* Check if the decompressed sub-block was not
+				 * full-length. */
+				if (dest < dest_sb_end) {
+					int nr_bytes = dest_sb_end - dest;
 
-			ntfs_log_debug("Filling incomplete sub-block with zeroes.\n");
-			/* Zero remainder and update destination position. */
-			memset(dest, 0, nr_bytes);
-			dest += nr_bytes;
-		}
-		/* We have finished the current sub-block. */
-		goto do_next_sb;
-	}
-	/* Check we are still in range. */
-	if (cb > cb_sb_end || dest > dest_sb_end)
-		goto return_overflow;
-	/* Get the next tag and advance to first token. */
-	tag = *cb++;
-	/* Parse the eight tokens described by the tag. */
-	for (token = 0; token < 8; token++, tag >>= 1) {
-		u16 lg, pt, length, max_non_overlap;
-		register u16 i;
-		u8 *dest_back_addr;
+					ntfs_log_debug("Filling incomplete "
+						"sub-block with zeroes.\n");
+					/* Zero remainder and update destination
+					 * position. */
+					memset(dest, 0, nr_bytes);
+					dest += nr_bytes;
+				}
+				/* We have finished the current sub-block. */
+				goto do_next_sb;
+			}
+			/* Check we are still in range. */
+			if (cb > cb_sb_end || dest > dest_sb_end)
+				goto return_overflow;
+			/* Get the next tag and advance to first token. */
+			tag = *cb++;
+			/* Parse the eight tokens described by the tag. */
+			for (token = 0; token < 8; token++, tag >>= 1) {
+				u16 lg, pt, length, max_non_overlap;
+				register u16 i;
+				u8 *dest_back_addr;
 
-		/* Check if we are done / still in range. */
-		if (cb >= cb_sb_end || dest > dest_sb_end)
-			break;
-		/* Determine token type and parse appropriately.*/
-		if ((tag & NTFS_TOKEN_MASK) == NTFS_SYMBOL_TOKEN) {
-			/*
-			 * We have a symbol token, copy the symbol across, and
-			 * advance the source and destination positions.
-			 */
-			*dest++ = *cb++;
-			/* Continue with the next token. */
-			continue;
+				/* Check if we are done / still in range. */
+				if (cb >= cb_sb_end || dest > dest_sb_end)
+					break;
+				/* Determine token type and parse
+				 * appropriately. */
+				if ((tag & NTFS_TOKEN_MASK) ==
+						NTFS_SYMBOL_TOKEN)
+				{
+					/*
+					 * We have a symbol token, copy the
+					 * symbol across, and advance the source
+					 * and destination positions.
+					 */
+					*dest++ = *cb++;
+					/* Continue with the next token. */
+					continue;
+				}
+				/*
+				 * We have a phrase token. Make sure it is not
+				 * the first tag in the sb as this is illegal
+				 * and would confuse the code below.
+				 */
+				if (dest == dest_sb_start)
+					goto return_overflow;
+				/*
+				 * Determine the number of bytes to go back (p)
+				 * and the number of bytes to copy (l). We use
+				 * an optimized algorithm in which we first
+				 * calculate log2(current destination position
+				 * in sb), which allows determination of l and p
+				 * in O(1) rather than O(n). We just need an
+				 * arch-optimized log2() function now.
+				 */
+				lg = 0;
+				for (i = dest - dest_sb_start - 1; i >= 0x10;
+						i >>= 1)
+				{
+					lg++;
+				}
+				/* Get the phrase token into i. */
+				pt = le16_to_cpup((le16*)cb);
+				/*
+				 * Calculate starting position of the byte
+				 * sequence in the destination using the fact
+				 * that p = (pt >> (12 - lg)) + 1 and make sure
+				 * we don't go too far back.
+				 */
+				dest_back_addr = dest - (pt >> (12 - lg)) - 1;
+				if (dest_back_addr < dest_sb_start)
+					goto return_overflow;
+				/* Now calculate the length of the byte
+				 * sequence. */
+				length = (pt & (0xfff >> lg)) + 3;
+				/* Verify destination is in range. */
+				if (dest + length > dest_sb_end)
+					goto return_overflow;
+				/* The number of non-overlapping bytes. */
+				max_non_overlap = dest - dest_back_addr;
+				if (length <= max_non_overlap) {
+					/* The byte sequence doesn't overlap,
+					 * just copy it. */
+					memcpy(dest, dest_back_addr, length);
+					/* Advance destination pointer. */
+					dest += length;
+				} else {
+					/*
+					 * The byte sequence does overlap, copy
+					 * non-overlapping part and then do a
+					 * slow byte by byte copy for the
+					 * overlapping part. Also, advance the
+					 * destination pointer.
+					 */
+					memcpy(dest, dest_back_addr,
+						max_non_overlap);
+					dest += max_non_overlap;
+					dest_back_addr += max_non_overlap;
+					length -= max_non_overlap;
+					while (length--)
+						*dest++ = *dest_back_addr++;
+				}
+				/* Advance source position and continue with the
+				 * next token. */
+				cb += 2;
+			}
+			/* No tokens left in the current tag. Continue with the
+			 * next tag. */
+			goto do_next_tag;
 		}
-		/*
-		 * We have a phrase token. Make sure it is not the first tag in
-		 * the sb as this is illegal and would confuse the code below.
-		 */
-		if (dest == dest_sb_start)
-			goto return_overflow;
-		/*
-		 * Determine the number of bytes to go back (p) and the number
-		 * of bytes to copy (l). We use an optimized algorithm in which
-		 * we first calculate log2(current destination position in sb),
-		 * which allows determination of l and p in O(1) rather than
-		 * O(n). We just need an arch-optimized log2() function now.
-		 */
-		lg = 0;
-		for (i = dest - dest_sb_start - 1; i >= 0x10; i >>= 1)
-			lg++;
-		/* Get the phrase token into i. */
-		pt = le16_to_cpup((le16*)cb);
-		/*
-		 * Calculate starting position of the byte sequence in
-		 * the destination using the fact that p = (pt >> (12 - lg)) + 1
-		 * and make sure we don't go too far back.
-		 */
-		dest_back_addr = dest - (pt >> (12 - lg)) - 1;
-		if (dest_back_addr < dest_sb_start)
-			goto return_overflow;
-		/* Now calculate the length of the byte sequence. */
-		length = (pt & (0xfff >> lg)) + 3;
-		/* Verify destination is in range. */
-		if (dest + length > dest_sb_end)
-			goto return_overflow;
-		/* The number of non-overlapping bytes. */
-		max_non_overlap = dest - dest_back_addr;
-		if (length <= max_non_overlap) {
-			/* The byte sequence doesn't overlap, just copy it. */
-			memcpy(dest, dest_back_addr, length);
-			/* Advance destination pointer. */
-			dest += length;
-		} else {
-			/*
-			 * The byte sequence does overlap, copy non-overlapping
-			 * part and then do a slow byte by byte copy for the
-			 * overlapping part. Also, advance the destination
-			 * pointer.
-			 */
-			memcpy(dest, dest_back_addr, max_non_overlap);
-			dest += max_non_overlap;
-			dest_back_addr += max_non_overlap;
-			length -= max_non_overlap;
-			while (length--)
-				*dest++ = *dest_back_addr++;
-		}
-		/* Advance source position and continue with the next token. */
-		cb += 2;
 	}
-	/* No tokens left in the current tag. Continue with the next tag. */
-	goto do_next_tag;
 return_overflow:
 	errno = EOVERFLOW;
 	ntfs_log_perror("Failed to decompress file");
